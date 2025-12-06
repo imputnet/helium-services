c631dca72fd47d39636cf60b0aaa93573187d564
jj <log@riseup.net>
2025-12-06 07:20:10 +0000

    svc/bangs: uncomment trailing comma regex

--- svc/bangs/mkbangs.mjs
+++ svc/bangs/mkbangs.mjs
@@ -170,8 +170,7 @@ const print = ({ bangs, license }) => {
         ...commentLicense, '',
         JSON.stringify(bangs, null, 1)
             .replace(/\[\s*\n\s*(".*?")\s*\n\s*\],/g, '[ $1 ],') // fold one-elem array onto single line
-        /* todo: uncomment this after 0.5.x:
-            .replace(/"(\n\s*])/g, '",\$1') */
+            .replace(/"(\n\s*])/g, '",\$1') // add trailing commas to multi-element trigger arrays
     ].join('\n');
 }
 
