5f6cef0a32b30a6459f12fadcc09ba7f40b90113
jj <log@riseup.net>
2026-01-26 15:36:30 +0000

    svc/ubo: bump assets version

--- svc/ubo/lib/assets-info.ts
+++ svc/ubo/lib/assets-info.ts
@@ -1,12 +1,12 @@
 import { env } from './env.ts';
 
-const VERSION_HELIUM = '1.68.1rc2';
-const VERSION_VANILLA = '1.68.0';
+const VERSION_HELIUM = '1.69.0';
+const VERSION_VANILLA = '1.69.0';
 
 const CSUM_HELIUM =
     'e1b8fe8fca5f81d1bfb9e149b82dfb077624329f4e4a75603dd7d23534ea142d';
 const CSUM_VANILLA =
-    'cad897c8cbf81b0fce2a08a5382baee9397c6563a6391329030aa52864c329ca';
+    'bb082033a0f52ac7b22217ecbfe3e53f5b080732ec959f81d2e6cbe8df5a53a5';
 
 const VERSION = env.useHeliumAssets ? VERSION_HELIUM : VERSION_VANILLA;
 const REPO = env.useHeliumAssets ? 'imputnet/uBlock' : 'gorhill/uBlock';
