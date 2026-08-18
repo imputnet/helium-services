108c73ed857bd587c69a5673ee7297b5ba7568c2
jj <log@riseup.net>
2026-08-18 12:58:32 +0000

    ubo-filters: update from upstream

--- svc/ubo/lib/assets-info.ts
+++ svc/ubo/lib/assets-info.ts
@@ -1,12 +1,12 @@
 import { env } from './env.ts';
 
-const VERSION_HELIUM = '1.71.0';
-const VERSION_VANILLA = '1.71.0';
+const VERSION_HELIUM = '1.73.0';
+const VERSION_VANILLA = '1.73.0';
 
 const CSUM_HELIUM =
-    '38252894162bf0cc9ed682669760922c17af67d9a1bd27b082997d732895afd0';
+    '8223523350062ecc8406265fbf2dfbf22aede1b5e3d1e1f83c311cec4db36076';
 const CSUM_VANILLA =
-    '5107ce702293e110ce6cc6467a51e689e919eed4382650c354c1d66db2aacc3d';
+    'fff077d6a1f5522170fa343008e18a4e41228fae8dd5abe5796191f7f3400b17';
 
 const VERSION = env.useHeliumAssets ? VERSION_HELIUM : VERSION_VANILLA;
 const REPO = env.useHeliumAssets ? 'imputnet/uBlock' : 'gorhill/uBlock';
